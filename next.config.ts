bc793d40462c3381bf0848ef3eae05b9b27c9818
Ale <alejandro79861@gmail.com>
2025-08-09 16:22:49 -0500

    Add image configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.URL_IMAGES_CONFIG || "",
+        port: "",
+        pathname: "/t/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
