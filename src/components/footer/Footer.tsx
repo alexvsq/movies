973484ef580751be67d7d84f89f889e990551853
Ale <alejandro79861@gmail.com>
2025-08-09 19:54:07 -0500

    Refactor Footer component to remove Logo and improve layout

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,18 +1,19 @@
-import React from "react";
-import Logo from "@/components/shared/Logo";
-
 export default function Footer() {
   return (
     <footer className="h-52 w-full ">
       <div className="container-dynamic flex items-center justify-between h-full">
-        <Logo />
+        <div className="flex flex-col gap-2 items-center">
+          <a href="https://www.themoviedb.org" target="_blank">
+            <img src="/tmdb-logo.svg" alt="tmdb logo" className="h-6" />
+          </a>
+          <p className="text-xs text-text-secondary">API by tmdb</p>
+        </div>
 
         <aside>
           Proyect by{" "}
           <a
             href="https://portfolio-six-sooty-75.vercel.app/en/"
             target="_blank"
-            rel="noreferrer"
             className="text-primary font-semibold underline"
           >
             AlexVsq
