e83a205e7155d3b9f166ccc8aebc04faf26dca01
Ale <alejandro79861@gmail.com>
2025-08-09 20:33:31 -0500

    Refactor Nav component to improve layout and add Menu icon

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,15 +1,15 @@
 import Logo from "@/components/shared/Logo";
-import { Search } from "lucide-react";
+import { Search, Menu } from "lucide-react";
 import NavItems from "./navItems";
 import Link from "next/link";
 
 export default function Nav() {
   return (
     <div className="w-full flex justify-center fixed z-20 nav-scroll-bg">
-      <nav className="container-dynamic py-4 flex flex-col gap-4 md:flex-row items-center justify-between w-full">
+      <nav className="container-dynamic py-4 flex flex-row items-center justify-between w-full">
         <Logo />
 
-        <aside className="flex items-center gap-8">
+        <aside className="flex items-center gap-4 md:gap-8">
           <NavItems />
           <div className="w-[1px] h-5 bg-white" />
           <Link href="/search">
