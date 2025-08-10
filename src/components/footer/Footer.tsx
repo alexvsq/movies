a71554c36ebcf43ffaf0bc6570fa7354e787ca56
Ale <alejandro79861@gmail.com>
2025-08-09 19:49:04 -0500

    Refactor Footer component layout and add project attribution

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import Logo from "@/components/shared/Logo";
 
 export default function Footer() {
   return (
-    <footer className="h-52 w-full flex items-center justify-center">
-      <p>Footer</p>
+    <footer className="h-52 w-full ">
+      <div className="container-dynamic flex items-center justify-between h-full">
+        <Logo />
+
+        <aside>
+          Proyect by{" "}
+          <a
+            href="https://portfolio-six-sooty-75.vercel.app/en/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-primary font-semibold underline"
+          >
+            AlexVsq
+          </a>
+        </aside>
+      </div>
     </footer>
   );
 }
