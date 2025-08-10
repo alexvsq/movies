7b2420cfa6f55a205076a0f20bfa258dd0f6316a
Ale <alejandro79861@gmail.com>
2025-08-09 21:08:10 -0500

    Add error and not-found components for improved user feedback

---
+++ src/app/error.tsx
@@ -0,0 +1,7 @@
+export default function error() {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <h3 className="text-3xl font-semibold text-primary">Error</h3>
+    </div>
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function notFound() {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <h3 className="text-3xl font-semibold text-primary">Not Found</h3>
+    </div>
+  );
+}
