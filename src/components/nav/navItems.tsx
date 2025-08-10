14da9bf387e1f756ff348481aaf7d28b380f7c39
Ale <alejandro79861@gmail.com>
2025-08-09 20:33:37 -0500

    Add icons to navigation items for improved visual representation

--- src/components/nav/navItems.tsx
+++ src/components/nav/navItems.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { usePathname } from "next/navigation";
+import { Tv, Clapperboard, House } from "lucide-react";
 
 export default function navItems() {
   const pathname = usePathname();
@@ -8,31 +9,35 @@ export default function navItems() {
     {
       name: "Home",
       href: "/",
+      icon: <House />,
     },
     {
       name: "Movies",
       href: "/movies",
+      icon: <Clapperboard />,
     },
     {
       name: "Series",
       href: "/series",
+      icon: <Tv />,
     },
   ];
 
   return (
-    <ul className="flex items-center gap-16 text-text-secondary">
+    <ul className="flex items-center gap-4 md:gap-16 text-text-secondary">
       {navButtons.map((navButton, index) => {
         return (
-          <a key={index} href={navButton.href} className="">
-            <li
-              className={`hover:text-white ${
-                pathname == navButton.href
-                  ? "underline underline-offset-8 text-white"
-                  : ""
-              }`}
-            >
-              {navButton.name}
-            </li>
+          <a
+            key={index}
+            href={navButton.href}
+            className={`hover:text-white ${
+              pathname == navButton.href
+                ? "underline underline-offset-8 text-white"
+                : ""
+            }`}
+          >
+            <li className="hidden md:block">{navButton.name}</li>
+            <li className="md:hidden">{navButton.icon}</li>
           </a>
         );
       })}
